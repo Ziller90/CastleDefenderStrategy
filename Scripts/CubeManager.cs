c19cd1d5137e77d7eed3ff8563728c23caa756c8
Ilya Zdorovtsov <zilleres@mail.ru>
2019-02-03 20:59:33 +0300

    Main folders was added, Grass and path cubes was added.

---
+++ Scripts/CubeManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeManager : MonoBehaviour
+{
+    public CubeScriptableObject Cube;
+    void Start()
+    {
+        gameObject.GetComponent<MeshRenderer>().material = Cube.CubeMaterial;
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Scripts/CubeScriptableObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCube", menuName = "Cube")]
+public class CubeScriptableObject : ScriptableObject
+{
+    public string CubeType;
+
+    public Material CubeMaterial;
+}
