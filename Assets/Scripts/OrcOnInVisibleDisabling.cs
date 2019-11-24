fe98298c6c314a15230c8f0af8876998cf7d607d
Ilya Zdorovtsov <zilleres@mail.ru>
2019-11-24 10:45:06 +0300

    New optimizated Hp bars

---
+++ Assets/Scripts/OrcOnInVisibleDisabling.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrcOnInVisibleDisabling : MonoBehaviour
+{
+    public GameObject Orc;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnBecameInvisible()
+    {
+        Orc.SetActive(false);
+    }
+    void OnBecameVisible()
+    {
+        Orc.SetActive(true);
+    }
+}
